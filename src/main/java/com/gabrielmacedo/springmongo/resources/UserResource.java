30381faa2b8b0afad7a80245c28ba0d7041cdf06
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-26 18:37:17 -0300

    feat: implements get User by id

--- src/main/java/com/gabrielmacedo/springmongo/resources/UserResource.java
+++ src/main/java/com/gabrielmacedo/springmongo/resources/UserResource.java
@@ -1,12 +1,12 @@
 package com.gabrielmacedo.springmongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +26,12 @@ public class UserResource {
 		List<User> users = userService.findAll();
 		List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());	
 		return ResponseEntity.ok().body(usersDTO);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok().body(userDTO);
 	}
 }
--- src/main/java/com/gabrielmacedo/springmongo/services/UserService.java
+++ src/main/java/com/gabrielmacedo/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gabrielmacedo.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabrielmacedo.springmongo.domain.User;
 import com.gabrielmacedo.springmongo.repository.UserRepository;
+import com.gabrielmacedo.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 	
 }
