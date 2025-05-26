f94d07c42a8b0176f547b7c8ce9be810b77e8359
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-26 17:48:25 -0300

    feat: create UserRepository and implements findAll

--- src/main/java/com/gabrielmacedo/springmongo/resources/UserResource.java
+++ src/main/java/com/gabrielmacedo/springmongo/resources/UserResource.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabrielmacedo.springmongo.domain.User;
+import com.gabrielmacedo.springmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User u1 = new User("1", "Gabriel Macedo", "gabriel@gmail.com");
-		User u2 = new User("2", "Vercel Rodrigues", "vercel@gmail.com");
-		List<User> list =  new ArrayList<>();
-		list.addAll(Arrays.asList(u1, u2));
-		
+		List<User> list = userService.findAll();	
 		return ResponseEntity.ok().body(list);
 	}
 }
---
+++ src/main/java/com/gabrielmacedo/springmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.gabrielmacedo.springmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielmacedo.springmongo.domain.User;
+import com.gabrielmacedo.springmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+	
+}
