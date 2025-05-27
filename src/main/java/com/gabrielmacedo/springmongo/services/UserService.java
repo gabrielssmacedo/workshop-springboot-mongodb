0d38524eb53939661551d58bec2fca256c603c19
gabrielssmacedo <gabrielmacedo317@gmail.com>
2025-05-27 10:48:56 -0300

    feat: implements update by id

--- src/main/java/com/gabrielmacedo/springmongo/services/UserService.java
+++ src/main/java/com/gabrielmacedo/springmongo/services/UserService.java
@@ -36,6 +36,17 @@ public class UserService {
 	    userRepository.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = userRepository.findById(user.getId()).get();
+		updateData(newUser, user);
+		return userRepository.save(newUser);
+	}
+	
+	public void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+	
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
